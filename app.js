1d48ec24af6461887483c55be9ee79fef3bbe06d
J-Hannes <Hannes_E@gmx.de>
2013-12-28 20:47:13 +0100

    Update to use Express 3.x

--- app.js
+++ app.js
@@ -2,9 +2,17 @@
 
 var express = require('express');
 var socket = require('socket.io');
+var http = require('http');
 
-var server = express.createServer();
+var app = express();
+var server = http.createServer(app);
 var io = socket.listen(server);
+
+server.listen(8080);
+
+app.get('/', function(req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
 
 io.sockets.on('connection', function() {
   console.log('Client connected...');
